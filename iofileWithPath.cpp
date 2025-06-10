612b7966a7c9fb4083986eca4e87450ba08b2884
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-06-10 10:49:01 +0700

    perbaiki input nama file dan getline setelah cin

---
+++ iofileWithPath.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   string baris;
+   string namafile;
+
+    cout << "memasukan nama file : ";
+    cin >> namafile;
+
+    ofstream outfile; 
+
+    outfile.open(namafile + ". txt", ios::out);
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
+    while (true)
+    {
+        cout << "- ";
+        getline(cin, baris);
+
+        if (baris == "q")
+        break;
+
+        outfile << baris << endl;
+    }
