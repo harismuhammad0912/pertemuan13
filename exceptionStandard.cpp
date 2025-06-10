3f6daf93a7bebefda80b36868c16c1e316f400ab
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-06-10 11:13:17 +0700

    tambah try block dengan akses out-of-range pada std::array

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -2,3 +2,12 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+int main ()
+{
+    cout << "awal program" << endl;
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        cout << data.at(4) << endl;
+    }
