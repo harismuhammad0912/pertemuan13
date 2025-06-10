232aa1b0e117f8c24f4156f36448acc918a499cd
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-06-10 11:08:18 +0700

    tambah blok try dengan throw exception

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        throw("halo");
+        cout << "pernyataan tidak akan dieksekusi " << endl;
+    }
